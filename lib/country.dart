3cd070e6531c04fb399fefb172dc2994a873d683
denixport <denixport@gmail.com>
2019-04-18 03:56:39 +0300

    More (c) headers

--- lib/country.dart
+++ lib/country.dart
@@ -1,6 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// Copyright (c) 2019, Denis Portnov. All rights reserved.
+// Released under MIT License that can be found in the LICENSE file.
 library country;
 
 export 'src/country.dart';
